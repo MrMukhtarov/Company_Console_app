02e72b063093e725131ff1ba066011fc2822f308
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-05-12 21:08:20 +0400

    create Company.cs file

---
+++ ConsoleProject.Core/Entities/Company.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleProject.Core.Entities;
+
+public class Company
+{
+    public int Id { get; }
+    private static int _id;
+    public string Name { get; set; }
+
+    public Company()
+    {
+        Id = _id;
+        _id++;
+    }
+}
