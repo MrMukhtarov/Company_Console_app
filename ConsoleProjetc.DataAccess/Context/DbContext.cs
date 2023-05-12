886e41645658a2ba91dbc380fb78f3fc122b963e
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-05-12 21:16:23 +0400

    create DbContext.cs file

---
+++ ConsoleProjetc.DataAccess/Context/DbContext.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleProject.Core.Entities;
+
+namespace ConsoleProjetc.DataAccess.Context;
+
+public static class DbContext
+{
+    public static List<Company> Companys { get; set; } = new List<Company>();
+    public static List<Department> Departments { get; set; } = new List<Department>();
+    public static List<Employee> Employees { get; set; } = new List<Employee>();
+}
