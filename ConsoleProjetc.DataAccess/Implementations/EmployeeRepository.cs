a9df47e940e52622d20afb9de6cc6b8d714c53e4
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-05-12 21:41:57 +0400

    create EmployeeRepository file

---
+++ ConsoleProjetc.DataAccess/Implementations/EmployeeRepository.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleProject.Core.Entities;
+using ConsoleProjetc.DataAccess.Context;
+using ConsoleProjetc.DataAccess.Interfaces;
+
+namespace ConsoleProjetc.DataAccess.Implementations;
+
+public class EmployeeRepository : IRepostory<Employee>
+{
+    public void Add(Employee entity)
+    {
+        DbContext.Employees.Add(entity);
+    }
+
+    public void Delete(Employee entity)
+    {
+        DbContext.Employees.Remove(entity);
+    }
+
+    public void Update(Employee entity)
+    {
+        Employee? employee = DbContext.Employees.Find(e => e.Id == entity.Id);
+        employee.Name = entity.Name;
+        employee.Surname = entity.Surname;
+        employee.Salary = entity.Salary;
+    }
+    public Employee? Get(int id)
+    {
+        return DbContext.Employees.Find(e => e.Id == id);
+    }
+
+    public List<Employee> GetAll()
+    {
+        return DbContext.Employees;
+    }
+
+}
--- ConsoleProjetc.DataAccess/Interfaces/IRepostory.cs
+++ ConsoleProjetc.DataAccess/Interfaces/IRepostory.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleProject.Core.Interfaces;
 
-namespace ConsoleProjetc.DataAccess.Interfaces
+namespace ConsoleProjetc.DataAccess.Interfaces;
+
+public interface IRepostory<T> where T : IEntitiy
 {
-    internal interface IRepostory
-    {
-    }
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    List<T> GetAll();
+    T? Get(int id);
 }
