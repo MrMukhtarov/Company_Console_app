2074f999d335f4c870b89d029f45df8c0c1a6c38
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-05-14 14:35:39 +0400

    modified program.cs file

--- ConsoleApplication.ConsoleAppUII/Program.cs
+++ ConsoleApplication.ConsoleAppUII/Program.cs
@@ -1,159 +1,111 @@
 ﻿using ConsoleProject.Business.Services;
 using ConsoleProject.Core.Entities;
 
-CompanyService company = new CompanyService();
-company.Create("NctLmn");
-company.Create("NctLmnnct");
-//company.Delete(2);
-company.Update("NctLmnnct", "myau");
-Console.WriteLine("GEt by id");
-Console.WriteLine(company.GetById(2));
+//CompanyService company = new CompanyService();
+//company.Create("NctLmn");
+//company.Create("NctLmnnct");
+////company.Delete(2);
+//company.Update("NctLmnnct", "myau");
+//Console.WriteLine("GEt by id");
+//Console.WriteLine(company.GetById(2));
 
-foreach (var i in company.GetAll())
-{
-    Console.WriteLine(i);
-}
-
-
-Console.WriteLine("==========");
-Console.WriteLine("Employee");
-Console.WriteLine("==========");
-Employee employee1 = new Employee("Nicat","Muxtarov",12500);
-Employee employee2 = new Employee("Leman", "Mammadly", 12500);
-Employee employee3 = new Employee("Lomon", "Muxtarova", 12500);
-EmployeeService employeeService = new EmployeeService();
-employeeService.Create(employee1);
-employeeService.Create(employee2);
-employeeService.Create(employee3);
-
-Console.WriteLine("==========");
-Console.WriteLine("Department");
-Console.WriteLine("==========");
-IDepartmentService department = new IDepartmentService();
-department.Create("Backend", 2, 2);
-department.Create("Frontend", 2, 2);
-department.UpdateDepartment("Frontend", "React", 3);
-department.AddEmployee(employee1, 2);
-department.AddEmployee(employee2, 2);
-department.AddEmployee(employee3, 2);
-foreach (var i in department.GetAll())
-{
-    Console.WriteLine(i);
-}
-
-Console.WriteLine("Myau departments");
-foreach (var i in company.GetAllDepartment("myau"))
-{
-    Console.WriteLine(i);
-}
-Console.WriteLine("GET Department Employee");
-foreach (var i in department.GetDepartmentEmployees("Reacts"))
-{
-    Console.WriteLine(i);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//using ConsoleProject.Business.Services;
-//using ConsoleProject.Core.Entities;
-
-//CompanyService companyService = new CompanyService();
-//companyService.Create("BErshka");
-//companyService.Create("BErshk");
-//companyService.Create("dolma");
-////companyService.Update("BErshka", "NEwyorker");
-////companyService.Update("BErshk", "KFC");
-////Console.WriteLine(companyService.GetById(1));
-
-////departmentService.Delete(0);
-////companyService.Delete(2);
-
-//foreach (Company i in companyService.GetAll())  
-//{
-//    Console.WriteLine($"{i.Id} {i.Name} {i.date}");
-//}
-//foreach(var i in companyService.GetAllDepartment("BErshka"))
+//foreach (var i in company.GetAll())
 //{
 //    Console.WriteLine(i);
 //}
-////IDepartmentService departmentService = new IDepartmentService();
-////departmentService.Create("Backend", 2, 2);
-////departmentService.Create("Forntend", 2, 2);
-////foreach(var i in departmentService.GetAll())
-////{
-////    Console.WriteLine(i.Name + " " + i.EmployeeLimit);
-////}
-////Console.WriteLine(departmentService.GetById(0));
 
-////EmployeeService employeeService = new EmployeeService();
 
-////Employee employee = new Employee("nicat"," ",15000);
-
-////Employee employee1 = new Employee("nicat"," ",15000);
-////employeeService.Create(employee);
-////departmentService.AddEmployee(employee1,0);
-////departmentService.AddEmployee(employee1,0);
-
-//IDepartmentService department = new IDepartmentService();
-//department.Create("Backend", 2, 1);
-//department.Create("Frontend", 2, 1);
-//department.Create("IT", 3, 1);
-
-//Employee employee1 = new Employee("Nicat","Muxtarov",125.59);
-//Employee employee2 = new Employee("Leman","Muxtarova",125.59);
-//Employee employee3 = new Employee("Leman","Muxtarova",125.59);
-//Employee employee4 = new Employee("Leman","Muxtarova",125.59);
-
+//Console.WriteLine("==========");
+//Console.WriteLine("Employee");
+//Console.WriteLine("==========");
+//Employee employee1 = new Employee("Nicat","Muxtarov",12500);
+//Employee employee2 = new Employee("Leman", "Mammadly", 12500);
+//Employee employee3 = new Employee("Lomon", "Muxtarova", 12500);
 //EmployeeService employeeService = new EmployeeService();
 //employeeService.Create(employee1);
 //employeeService.Create(employee2);
 //employeeService.Create(employee3);
-//employeeService.Create(employee4);
-//department.AddEmployee(employee1, 1);
-//department.AddEmployee(employee2, 1);
+
+//Console.WriteLine("==========");
+//Console.WriteLine("Department");
+//Console.WriteLine("==========");
+//IDepartmentService department = new IDepartmentService();
+//department.Create("Backend", 2, 2);
+//department.Create("Frontend", 2, 2);
+//department.UpdateDepartment("Frontend", "React", 3);
+//department.AddEmployee(employee1, 2);
+//department.AddEmployee(employee2, 2);
 //department.AddEmployee(employee3, 2);
-//employeeService.Update("Nijat", "Mukhtarov", 125000, 1, 2);
-//employeeService.Delete(1);
-//Console.WriteLine("BAckend");
-//foreach (var i in department.GetDepartmentEmployees("Backend"))
+//foreach (var i in department.GetAll())
 //{
 //    Console.WriteLine(i);
 //}
-//Console.WriteLine("Forntend");
-//foreach (var i in department.GetDepartmentEmployees("Frontend"))
+
+//Console.WriteLine("Myau departments");
+//foreach (var i in company.GetAllDepartment("myau"))
 //{
 //    Console.WriteLine(i);
 //}
-//Console.WriteLine("ALL");
-//foreach (var i in employeeService.GetAll())
+//Console.WriteLine("GET Department Employee");
+//foreach (var i in department.GetDepartmentEmployees("Reacts"))
 //{
 //    Console.WriteLine(i);
 //}
-//Console.WriteLine("GetById");
-//Console.WriteLine(employeeService.GetById(3));
-////department.UpdateDepartment("Backen", "Backend", 3);
+
+
+CompanyService companyService = new CompanyService();
+IDepartmentService departmentService = new IDepartmentService();
+EmployeeService employeeService = new EmployeeService();
+do
+{
+    Console.WriteLine("====Welcome Company Management System====\n");
+    Console.WriteLine("Enter number what you want opearation\n");
+    Console.WriteLine("1. Copmpany Yaratmaq");
+    Console.WriteLine("2. Compyany`de deyişiklik etmek");
+    Console.WriteLine("3. Company silmek");
+    Console.WriteLine("4. Bütün Company`lerin siyahısını çıxarmaq");
+    Console.WriteLine("5. ID`sine göre Company`lere baxmaq");
+    Console.WriteLine("6. Seçdiyiniz Company`de olan Departmentlerin siyasına baxmaq");
+    Console.WriteLine("7. Department yaratmaq");
+    Console.WriteLine("8. Departmentde deyisiklik etmek");
+    Console.WriteLine("9. Departmenti silmek");
+    Console.WriteLine("10. Butun Departmentlerin siyashisina baxmaq");
+    Console.WriteLine("11. ID`sine gore Departmente baxmaq");
+    Console.WriteLine("12. Departmente Employee elave etmek");
+    Console.WriteLine("13. Department adina gore iscilere baxmaq");
+    Console.WriteLine("14. Employee yaratmaq");
+    Console.WriteLine("15. Employee silmek");
+    Console.WriteLine("16. Employee`de deyisiklik etmek");
+    Console.WriteLine("17. Employee`lerin siyahisina baxmaq");
+    Console.WriteLine("18. ID`sine gore Employee axtarmaq");
+    string choose = Console.ReadLine();
+    int chooseNum;
+} while (false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
